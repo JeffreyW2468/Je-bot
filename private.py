d4697285a63776b06f66c300357e4fdc795d9ad2
JeffreyW2468 <70827087+JeffreyW2468@users.noreply.github.com>
2021-03-26 23:17:56 -0700

    Update private.py

--- private.py
+++ private.py
@@ -1,18 +1,23 @@
-if message.content.startswith('anime pls'):
-        await message.channel.send('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvDhcBWIQYlh1tUAtslgBO5gueF05IWaD0Ww&usqp=CAU')
-        await message.channel.send('moshi moshi')
-        await message.channel.send('.')
-        await message.channel.send('doki')
-        await message.channel.send('doki')
-        await message.channel.send('watashi wa anime gorl kyun <3')
+import discord
+import os
+from discord import message
 
-if message.content.startswith('penis'):
-        await message.channel.send('nono {} u naughty boi'.format(message.author.mention))
+async def sussus_amogus():
+    if message.content.startswith('anime pls'):
+            await message.channel.send('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvDhcBWIQYlh1tUAtslgBO5gueF05IWaD0Ww&usqp=CAU')
+            await message.channel.send('moshi moshi')
+            await message.channel.send('.')
+            await message.channel.send('doki')
+            await message.channel.send('doki')
+            await message.channel.send('watashi wa anime gorl kyun <3')
 
-#;f notif    
+    if message.content.startswith('penis'):
+            await message.channel.send('nono {} u naughty boi'.format(message.author.mention))
+
+    #;f notif    
     '''if message.content.startswith(';f'):
         await message.channel.send('ara ara the fish will not bite today ')'''
 
-#;p ping    
+    #;p ping    
     '''if message.content.startswith(';p'):
         await message.channel.send('I am watching you {}'.format(message.author.mention))'''
